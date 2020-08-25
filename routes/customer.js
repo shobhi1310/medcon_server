7702dd25b30a41a882c61ce324cfcc29f3fe85db
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-08-26 00:59:19 +0530

    some template added in customer.js

--- routes/customer.js
+++ routes/customer.js
@@ -1,4 +1,8 @@
 const router = require('express').Router()
 const customerModel = require('../models/Customer.model')
 
+router.route('/login').post((req,res)=>{})
+
+router.route('/register').post((req,res)=>{})
+
 module.exports = router
