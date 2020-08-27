cad9743464e3f45fd7469276d12d8c481e6efdba
Mir Sameed Ali <mirsameedali@Mirs-MacBook-Pro.local>
2020-08-27 23:49:56 +0530

    Medicine Update Route

--- models/Shop.model.js
+++ models/Shop.model.js
@@ -1,34 +1,39 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const shopSchema = new Schema({
-    name:{type:String,required:true},
-    email_id:{type:String},
-    password:{type:String},
-    address:{type:String},
-    location:[
-        {type:Schema.Types.Number},
-        {type:Schema.Types.Number}
+const shopSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email_id: { type: String },
+    password: { type: String },
+    address: { type: String },
+    location: [{ type: Schema.Types.Number }, { type: Schema.Types.Number }],
+    licence: { type: String },
+    phone: { type: String },
+    booking_current: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'bookings',
+      },
     ],
-    licence:{type:String},
-    phone:{type:String},
-    booking_current:[{
-        type:Schema.Types.ObjectId,
-        ref:'bookings'
-    }],
-    booking_history:[{
-        type:Schema.Types.ObjectId,
-        ref:'bookings'
-    }],
-    medicines:[{
-        type:Schema.Types.ObjectId,
-        // status:{type:Boolean},
-        ref:'medicines'
-    }]
-},{
-    timestamps:true
-});
+    booking_history: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'bookings',
+      },
+    ],
+    medicines: [
+      {
+        medicine: { type: Schema.Types.ObjectId, ref: 'medicines' },
+        status: { type: Boolean },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Shops = mongoose.model('shops',shopSchema);
+const Shops = mongoose.model('shops', shopSchema);
 
-module.exports = Shops
+module.exports = Shops;
--- routes/shop.js
+++ routes/shop.js
@@ -1,26 +1,53 @@
-const router = require('express').Router()
-const shopModel = require('../models/Shop.model')
+const router = require('express').Router();
+const shopModel = require('../models/Shop.model');
 
-router.route('/:id').get(async (req,res)=>{
-    const id = req.params.id
-    let shop
-    try {
-        shop = await shopModel.findById(id)
-        res.json(shop)
-    } catch (error) {
-        res.json(error)
-    }
-})
+router.route('/:id').get(async (req, res) => {
+  const id = req.params.id;
+  let shop;
+  try {
+    shop = await shopModel.findById(id);
+    res.json(shop);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
-router.route('/medicinelist/:id').get(async (req,res)=>{
-    const id = req.params.id
-    let shop
-    try {
-        shop = await shopModel.findById(id,{medicines:1}).populate('medicines')
-        res.json(shop)
-    } catch (error) {
-        res.json(error)
-    }
-})
+router.route('/medicinelist/:id').get(async (req, res) => {
+  const id = req.params.id;
+  let shop;
+  try {
+    shop = await shopModel
+      .findById(id, { medicines: 1 })
+      .populate('medicines.medicine');
+    res.json(shop);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
-module.exports = router
+router.post('/:ShopID/update/:MedicineID', (req, res) => {
+  const shopID = req.params.ShopID;
+  const medicineID = req.params.MedicineID;
+  try {
+    shopModel.findById(shopID).then((Shop) => {
+      //console.log(Shop);
+      for (let i = 0; i < Shop.medicines.length; i++) {
+        //console.log(Shop.medicines[i].medicine, medicineID);
+        if (Shop.medicines[i].medicine == medicineID) {
+          //console.log('updated');
+          Shop.medicines[i].status = !Shop.medicines[i].status;
+          break;
+        }
+      }
+      Shop.save();
+    });
+
+    res.status(200).json({
+      message: 'updated',
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+module.exports = router;
