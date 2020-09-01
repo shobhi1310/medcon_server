da17938ff85566248a040827bba82c50cfe505df
chirag2706 <cs18b006@iittp.ac.in>
2020-09-01 23:51:26 +0530

    customer update

--- routes/user.js
+++ routes/user.js
@@ -74,6 +74,59 @@ router.route('/:id').get(async(req, res) => {
 
 router.route('/profile/update/:id').post(async (req,res)=>{
 
+    //right now i am thinking that only user is updating its profile
+    //i assuming customer can change its name,password and phone no
+    //he/she can't change its email-id
+    let user = {};
+
+    try{
+        user = await customerModel.findById(req.params.id);
+
+        console.log("User inside customer profile update route")
+        console.log(user);
+
+        // res.status(200).json(user);
+
+        var newName = user.name;
+        var newPhone = user.phone;
+        var newPassword = user.password;
+
+
+        if(req.body.name!==null && req.body.name!==undefined && req.body.name.length>0){
+            newName = req.body.name;
+        }
+
+
+        if(req.body.phone!==null && req.body.phone!==undefined && req.body.phone.length>0){
+            newPhone = req.body.phone;
+        }
+
+
+        if(req.body.password!==null && req.body.password!==undefined && req.body.password.length>0){
+            newPassword = req.body.password;
+        }
+
+
+
+        var Query = { _id: req.params.id };
+        var updateValue = { $set: { name: newName , phone: newPhone, password:newPassword} };
+
+
+        customerModel.updateOne(Query,updateValue,(err,response)=>{
+            if(err){
+                throw err;
+                // return;
+            }
+
+            
+        })
+
+        res.json({"Success":"Customer Profile has been updated successfully"});
+
+    }catch(error){
+        res.status(404).json(error);
+    }
+
 })
 
 module.exports = router
