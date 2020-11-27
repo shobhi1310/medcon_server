30b998805c1d878132e7ea4a9365d80a3b69cc45
chirag2706 <cs18b006@iittp.ac.in>
2020-11-27 20:11:29 +0530

    few changes done

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const uri = process.env.URI;
 // const uri = "mongodb://localhost:27017/medconnect";
 
-// mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
 
 const connection = mongoose.connection
 connection.once('open', () => {
